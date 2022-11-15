b6adf854bd40d96009dd1335d738042f3d779a26
bimbi <fadhil.bima74@gmail.com>
2022-11-15 12:06:06 +0700

    sephora_home

--- lib/home/home_widgets/card_menu.dart
+++ lib/home/home_widgets/card_menu.dart
@@ -10,10 +10,16 @@ class CardMenu extends StatelessWidget {
       child: Container(
         height: 80,
         margin: EdgeInsets.symmetric(horizontal: 13),
-        decoration: BoxDecoration(
-          color: Colors.red,
-          borderRadius: BorderRadius.circular(15)
-        ),
+          decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(15),
+            boxShadow: [
+              BoxShadow(
+                color: Color(0xFFBCC8E7),
+                blurRadius: 10
+              )
+            ]
+          ),
         child: Container(
           margin: EdgeInsets.symmetric(horizontal: 15, vertical: 15),
           height: 51,
@@ -23,13 +29,13 @@ class CardMenu extends StatelessWidget {
               Container(
                 height: 51,
                 width: 55,
-                color: Colors.green,
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
                     Container(
                       height: 25,
                       width: 25,
+                      margin: EdgeInsets.only(top: 2),
                       child: Image.asset('assets/icons/transfer.png'),
                     ),
                     Text('Transfer', style: TextStyle(fontSize: 11, fontWeight: FontWeight.w400),)
@@ -39,17 +45,50 @@ class CardMenu extends StatelessWidget {
               Container(
                 height: 51,
                 width: 55,
-                child: Image.asset('assets/icons/wallet.png'),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Container(
+                      height: 25,
+                      width: 25,
+                      margin: EdgeInsets.only(top: 2),
+                      child: Image.asset('assets/icons/wallet.png'),
+                    ),
+                    Text('E-Wallet', style: TextStyle(fontSize: 11, fontWeight: FontWeight.w400),)
+                  ],
+                ),
               ),
               Container(
                 height: 51,
                 width: 55,
-                child: Image.asset('assets/icons/cart.png'),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Container(
+                      height: 25,
+                      width: 25,
+                      margin: EdgeInsets.only(top: 2),
+                      child: Image.asset('assets/icons/cart.png'),
+                    ),
+                    Text('Pembelian', style: TextStyle(fontSize: 11, fontWeight: FontWeight.w400),)
+                  ],
+                ),
               ),
               Container(
                 height: 51,
                 width: 55,
-                child: Image.asset('assets/icons/multimedia.png'),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Container(
+                      height: 25,
+                      width: 25,
+                      margin: EdgeInsets.only(top: 2),
+                      child: Image.asset('assets/icons/multimedia.png'),
+                    ),
+                    Text('Lainya', style: TextStyle(fontSize: 11, fontWeight: FontWeight.w400),)
+                  ],
+                ),
               )
             ],
           ),
--- lib/home/screens/home_page_inactivated.dart
+++ lib/home/screens/home_page_inactivated.dart
@@ -1,5 +1,10 @@
+import 'package:animated_bottom_navigation_bar/animated_bottom_navigation_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:sephora_project/home/home_widgets/bank_card.dart';
+import 'package:sephora_project/home/home_widgets/curved_bottom_bar.dart';
+import 'package:sephora_project/home/home_widgets/daftar_favorit.dart';
+import 'package:sephora_project/home/home_widgets/history_display.dart';
+import 'package:sephora_project/home/home_widgets/history_item.dart';
 import 'package:sephora_project/home/home_widgets/user_profile.dart';
 import 'package:sephora_project/home/home_widgets/warning_inactivated.dart';
 
@@ -11,6 +16,7 @@ class HomieInactivated extends StatefulWidget {
 }
 
 class _HomieInactivatedState extends State<HomieInactivated> {
+  var _bottomActive = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -31,20 +37,84 @@ class _HomieInactivatedState extends State<HomieInactivated> {
             child: SingleChildScrollView(
                 scrollDirection: Axis.vertical,
                 child: Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                  padding: EdgeInsets.only(top: 10, left: 20, right: 20, bottom: 100),
                   child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
                     children: [
                       WarningInactivated(),
                       SizedBox(height: 19),
-                      BankCard()
+                      BankCard(),
+                      SizedBox(height: 40,),
+                      Container(
+                        height: 121,
+                        child: DaftarFavorit(),
+                      ),
+                      SizedBox(height: 50),
+                      HistoryDisp(),
                     ],
                   ),
                 )
             ),
-          )
+          ),
         ],
-      )
+      ),
+      extendBody: true,
+      bottomNavigationBar: BottomAppBar(
+        shape: CircularNotchedRectangle(),
+        notchMargin: 12,
+        child: Container(
+          margin: EdgeInsets.symmetric(horizontal: 20),
+          height: 90,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              NavbarTextIcon(
+                  icon: Icon(Icons.home, size: 30,), label: Text('Beranda'), onPress: (){}
+              ),
+              NavbarTextIcon(
+                  icon: Icon(Icons.receipt_rounded, size: 30,), label: Text('Mutasi'), onPress: (){}
+              ),
+              NavbarTextIcon(
+                  icon: Container(height: 24, width: 24,), label: Text('Transfer'), onPress: (){}
+              ),
+              NavbarTextIcon(
+                  icon: Icon(Icons.star_border_rounded, size: 30,), label: Text('Favorite'), onPress: (){}
+              ),
+              NavbarTextIcon(
+                  icon: Icon(Icons.person_outline_rounded, size: 30,), label: Text('Profile'), onPress: (){}
+              ),
+            ],
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){}, 
+        child: Icon(Icons.add),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+    );
+  }
+}
+
+class NavbarTextIcon extends StatelessWidget {
+  NavbarTextIcon({Key? key, this.icon, required this.label, required this.onPress}) : super(key: key);
+  late Text label;
+  final VoidCallback onPress;
+  Widget? icon;
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onPress,
+      child: Container(
+        height: 51,
+        width: 54,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            icon!,
+            label,
+          ],
+        ),
+      ),
     );
   }
 }
